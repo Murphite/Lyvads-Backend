797488781dc01b9cb44da78b7020e8c5846cd59a
Murphy Ogbeide <ogbeidemurphy@gmail.com>
2024-12-28 09:45:15 +0100

    Updated-Regualar-User-Controller

--- Lyvads.Application/Dtos/RegularUserDtos/RegularDto.cs
+++ Lyvads.Application/Dtos/RegularUserDtos/RegularDto.cs
@@ -6,8 +6,8 @@ public class RegularUserDto
 {
     public string? UserId { get; set; } 
     public string? FullName { get; set; } 
-    public string? Username { get; set; } 
+    public string? Email { get; set; } 
     public string? ProfilePictureUrl { get; set; } 
-    public DateTime CreatedAt { get; set; } 
+    public DateTimeOffset CreatedAt { get; set; } 
     public string? AppUserName { get; set; } 
 }
